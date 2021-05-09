2abaf7818c39c51e282569dcc988ab70d4d665e5
yuchiyama <>
2021-05-10 01:38:09 +0900

    iidx commit 210510-01

--- scoredata/admin.py
+++ scoredata/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Scoresp, Scoredp
+from .models import Scoresp, Scoredp, CsvUpload, VersionList
 
 # Register your models here.
 
 admin.site.register(Scoresp)
 admin.site.register(Scoredp)
+admin.site.register(CsvUpload)
+admin.site.register(VersionList)
